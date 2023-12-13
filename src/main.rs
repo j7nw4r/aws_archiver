c446a0e71ee7658641bf126136378553b31bf6cb
Johnathan W <12262567+j7nw4r@users.noreply.github.com>
2023-12-13 16:28:36 -0500

    More changes

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,42 @@
-use anyhow::bail;
-use aws_config::BehaviorVersion;
+use std::ops::Add;
+use byte_unit::{Byte, Unit};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()>{
-    let config = aws_config::load_defaults(BehaviorVersion::v2023_11_09()).await;
+    let config = aws_config::load_from_env().await;
     let client = aws_sdk_s3::Client::new(&config);
 
-    // List addressable buckets
-    println!("Buckets:");
-    let buckets = match client.list_buckets().send().await {
-        Ok(buckets) => buckets,
-        Err(e) => bail!("{}", e),
-    };
-    for bucket in buckets.buckets() {
-        let Some(ref name) = bucket.name else {
-            continue;
-        };
-        println!("{}", name)
-    }
-
     // List all objects within the Klothed production bucket.
-    let klothed_bucket = "klthd-storage-prod".to_string();
-    let list_objects_output = match client.list_objects_v2().bucket(klothed_bucket).send().await {
-        Ok(list_objects_output) => list_objects_output,
-        Err(e) => bail!("{}", e),
-    };
-    println!("Objects:");
-    for (count, obj) in list_objects_output.contents().iter().enumerate() {
-        if count >= 1000 {
+    let mut continuation_token : Option<String> = None;
+    let mut total: u128 = 0;
+    loop {
+        let list_objects_output =  client.list_objects_v2().bucket("klthd-storage-prod")
+            .set_prefix(Some("retailer".to_string()))
+            .set_continuation_token(continuation_token)
+            .send().await?;
+        for content in list_objects_output.contents() {
+            let Some(key) = content.key() else {
+                continue
+            };
+            if !key.ends_with("/images/image.png") {
+                continue
+            }
+            let Some(size) = content.size() else {
+                continue
+            };
+
+            total = total.add(1u128);
+            println!("{}", total);
+
+        }
+        // let byte = Byte::from_u128(total).unwrap_or_default();
+        // // let adj_byte = byte.get_adjusted_unit(Unit::GB);
+        // println!("{}", adj_byte.to_string());
+
+        if let None = list_objects_output.next_continuation_token() {
             break
         }
-        let key_name = obj.key?;
-        println!("Object Key: {}", key_name);
+        continuation_token = list_objects_output.next_continuation_token;
     }
 
     Ok(())
