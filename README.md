855ed3d3ce67b0c8ad2c5d4f17b40efc830be116
Johnathan W <12262567+j7nw4r@users.noreply.github.com>
2024-01-25 06:39:54 -0500

    done.

--- README.md
+++ README.md
@@ -3,6 +3,3 @@
 ## Purpose
 
 The `aws_archiver` is meant to retrieve selected images from Klothed S3 buckets for archival.
-
-## Running
-
--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,52 @@
-use std::ops::Add;
-use byte_unit::{Byte, Unit};
+use aws_sdk_s3::Client;
+use aws_sdk_s3::error::ProvideErrorMetadata;
+
+const BUCKET_NAME: &str = "klthd-storage-prod";
 
 #[tokio::main]
-async fn main() -> anyhow::Result<()>{
+async fn main() -> anyhow::Result<()> {
     let config = aws_config::load_from_env().await;
     let client = aws_sdk_s3::Client::new(&config);
 
-    // List all objects within the Klothed production bucket.
-    let mut continuation_token : Option<String> = None;
-    let mut total: u128 = 0;
+    delete_individually(client).await
+}
+
+async fn delete_individually(client: Client) -> anyhow::Result<()> {
+    let mut continuation_token: Option<String> = None;
     loop {
-        let list_objects_output =  client.list_objects_v2().bucket("klthd-storage-prod")
+        let list_objects_output = client
+            .list_objects_v2()
+            .bucket(BUCKET_NAME)
             .set_prefix(Some("retailer".to_string()))
             .set_continuation_token(continuation_token)
-            .send().await?;
+            .send()
+            .await?;
         for content in list_objects_output.contents() {
-            let Some(key) = content.key() else {
-                continue
-            };
-            if !key.ends_with("/images/image.png") {
-                continue
+            let Some(key) = content.key() else { continue };
+            if key.ends_with("/images/image.png") {
+                continue;
             }
-            let Some(size) = content.size() else {
-                continue
+
+            println!("Deleting: {}", key);
+            let _delete_object = match client
+                .delete_object()
+                .bucket(BUCKET_NAME)
+                .key(key)
+                .send()
+                .await
+            {
+                Ok(delete_object) => delete_object,
+                Err(e) => {
+                    println!("could not delete: {}\n (message: {})", key, e.message().unwrap_or_default());
+                    continue;
+                }
             };
-
-            total = total.add(1u128);
-            println!("{}", total);
-
         }
-        // let byte = Byte::from_u128(total).unwrap_or_default();
-        // // let adj_byte = byte.get_adjusted_unit(Unit::GB);
-        // println!("{}", adj_byte.to_string());
 
         if let None = list_objects_output.next_continuation_token() {
-            break
-        }
+            break;
+        };
+
         continuation_token = list_objects_output.next_continuation_token;
     }
 
